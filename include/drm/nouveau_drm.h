fbef8709c4d34a3d9e0f6c9f07f2d54c6659a510
Andrew Chew <achew@nvidia.com>
2015-07-20 11:22:04 -0700

    libdrm: Add drm_nouveau_gem_pushbuf_2
    Add drm_nouveau_gem_pushbuf_2 ioctl struct, pulled from chromeos-3.18
    kernel.
    
    Change-Id: I651e8d6ab91f17d63d391e8127a21a3af6e02af9
    Signed-off-by: Andrew Chew <achew@nvidia.com>

--- include/drm/nouveau_drm.h
+++ include/drm/nouveau_drm.h
@@ -171,6 +171,21 @@ struct drm_nouveau_gem_pushbuf {
 	uint64_t gart_available;
 };
 
+#define NOUVEAU_GEM_PUSHBUF_2_FENCE_WAIT                             0x00000001
+#define NOUVEAU_GEM_PUSHBUF_2_FENCE_EMIT                             0x00000002
+struct drm_nouveau_gem_pushbuf_2 {
+	uint32_t channel;
+	uint32_t flags;
+	uint32_t nr_push;
+	uint32_t nr_buffers;
+	int32_t  fence; /* in/out, depends on flags */
+	uint32_t pad;
+	uint64_t push; /* in raw hw format */
+	uint64_t buffers; /* ptr to drm_nouveau_gem_pushbuf_bo */
+	uint64_t vram_available;
+	uint64_t gart_available;
+};
+
 #define NOUVEAU_GEM_CPU_PREP_NOWAIT                                  0x00000001
 #define NOUVEAU_GEM_CPU_PREP_NOBLOCK                                 0x00000002
 #define NOUVEAU_GEM_CPU_PREP_WRITE                                   0x00000004
