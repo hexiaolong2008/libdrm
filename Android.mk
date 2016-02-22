e298f1f8f5094b8a7f1eccdaf97bf107b4b5db47
James Hawkins <jhawkins@google.com>
2016-02-22 15:18:37 -0800

    external/libdrm: Silence warnings.
    -Wno-enum-conversion
    -Wno-missing-field-initializers
    -Wno-pointer-arith
    -Wno-sign-compare
    -Wno-tautological-compare
    -Wno-unused-parameter
    
    Bug: 26936282
    Change-Id: I6c7857073e800dfd8490fa0a797e087f7bbb68b1

--- Android.mk
+++ Android.mk
@@ -26,7 +26,14 @@ LOCAL_PATH := $(call my-dir)
 # Import variables LIBDRM_{,H_,INCLUDE_H_,INCLUDE_VMWGFX_H_}FILES
 include $(LOCAL_PATH)/Makefile.sources
 
-common_CFLAGS := -DHAVE_LIBDRM_ATOMIC_PRIMITIVES=1
+common_CFLAGS := \
+    -DHAVE_LIBDRM_ATOMIC_PRIMITIVES=1 \
+    -Wno-enum-conversion \
+    -Wno-missing-field-initializers \
+    -Wno-pointer-arith \
+    -Wno-sign-compare \
+    -Wno-tautological-compare \
+    -Wno-unused-parameter
 
 # Static library for the device (recovery)
 include $(CLEAR_VARS)
